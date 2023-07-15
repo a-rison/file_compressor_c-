d94651f85054f0fb9f2a76eb45a4b7f5103a270b
Aryan Sinha <arison.aryan@gmail.com>
2023-07-15 11:38:55 +0530

    some small changes

--- compress.cpp
+++ compress.cpp
@@ -2,15 +2,23 @@
 #include "huffman.hpp"
 using namespace std;
 
+void compressFile(const string& inputFilePath, const string& outputFilePath){
+    huffman f(inputFilePath, outputFilePath);
+    f.compress();
+    cout << "Compressed successfully" << endl;
+}
+
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         cout << "Failed to detect Files";
 		exit(1);
 	}
     
-    huffman f(argv[1], argv[2]);
-    f.compress();
-    cout << "Compressed successfully" << endl;
+    string inputFilePath(argv[1]);
+    string outputFilePath(argv[2]);
+
+    compressFile(inputFilePath, outputFilePath);
     
     return 0;
 }
--- decompress.cpp
+++ decompress.cpp
@@ -2,15 +2,22 @@
 #include "huffman.hpp"
 using namespace std;
 
+void decompressFile(const string& inputFilePath,const string& outputFilePath){
+    huffman f(inputFilePath, outputFilePath);
+    f.decompress();
+    cout << "Decompressed successfully" << endl;
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         cout << "Failed to detect Files";
 		exit(1);
 	}
 
-    huffman f(argv[1], argv[2]);
-    f.decompress();
-    cout << "Decompressed successfully" << endl;
+    string inputFilePath(argv[1]);
+    string outputFilePath(argv[2]);
+
+    decompressFile(inputFilePath, outputFilePath);
 
     return 0;
 }
